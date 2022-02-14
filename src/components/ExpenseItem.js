aacd29b8a9ccff80f1b14ec4938b6c9bf0b7b4e9
AbinMuds <abinashmudbhari@gmail.com>
2022-02-14 03:53:23 -0700

    adds expenseItem.css and adds css for the ExpenseItem component and some changes made on it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmmout = 250.44;
+
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toDateString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmmout}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
