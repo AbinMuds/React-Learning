005394cd0d9770fdfa47071f2904952f6e61e8e8
AbinMuds <abinashmudbhari@gmail.com>
2022-02-16 17:43:24 -0700

    adds Expenses component and also its css to make app manageable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+  
   return (
     <div className='expense-item'>
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.date}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
