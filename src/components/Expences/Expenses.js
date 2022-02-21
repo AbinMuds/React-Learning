39f06ddf94b29ff2c5e43c2b13bda4ea4b35fa93
AbinMuds <abinashmudbhari@gmail.com>
2022-02-21 04:29:34 -0700

    adds filtered expenses show up when date is changed and also we can add expenses with form that will make the listing of the data possible

--- src/components/Expences/Expenses.js
+++ src/components/Expences/Expenses.js
@@ -7,19 +7,26 @@ import ExpenseFilter from "./ExpenseFilter";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState('2019');
-
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
-  }
+    }
+  const filterdExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
 
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-        <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-        <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
-        <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
+        {filterdExpenses.map(
+          (expenses) => 
+          <ExpenseItem 
+            key = {expenses.id}
+            title = {expenses.title}
+            amount = {expenses.amount}
+            date = {expenses.date}
+          />
+        )}
       </Card>
     </div>
     
